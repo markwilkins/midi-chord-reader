4b394d25a75f17e1958438c0e16362dbd79d08b9
Mark Wilkins <markwilkins@github.com>
2023-05-20 18:06:12 -0600

    MidiStore basics are working

--- src/MidiStore.cpp
+++ src/MidiStore.cpp
@@ -16,26 +16,21 @@ MidiStore::~MidiStore()
 
 /**
  * @brief Determine if any events are in this tree. Probably not useful.
- * 
- * @return bool true if it has children (e.g., there are note events, false if not) 
+ *
+ * @return bool true if it has children (e.g., there are note events, false if not)
  */
 bool MidiStore::hasData()
 {
     if (trackData->getNumChildren() > 0)
-    {
         return true;
-    }
     else
-    {
         return false;
-    }
 }
-
 
 /**
  * @brief Set the name of the object ... probably not useful
- * 
- * @param juce::String name 
+ *
+ * @param juce::String name
  */
 void MidiStore::setName(juce::String name)
 {
@@ -45,29 +40,27 @@ void MidiStore::setName(juce::String name)
 
 /**
  * @brief retrieve the name
- * 
- * @return juce::String 
+ *
+ * @return juce::String
  */
-juce::String MidiStore::getName() 
+juce::String MidiStore::getName()
 {
     static juce::Identifier propertyName("name");
     return trackData->getProperty(propertyName);
 }
 
-
 /**
  * @brief Add a midi note on/off event at the given time
- * 
+ *
  * @param int64 time   time info derived from AudioProcessor::processBlock callback
  * @param int   note   midi note number
  * @param bool  isOn   Is it an on or off event?
  */
 void MidiStore::addNoteEventAtTime(int64 time, int note, bool isOn)
 {
-    Identifier timeProp = notesAtIdent(time);
     const ScopedLock lock(storeLock);
     // Find the valuetree for this time (create it if it does not exist)
-    ValueTree child = trackData->getOrCreateChildWithName(timeProp, nullptr);
+    ValueTree child = ensureNoteEventAtTime(time);
 
     // Store the note with the identifier (the label) being the note number and the value being bool on/off
     Identifier noteProp(std::to_string(note));
@@ -79,33 +72,82 @@ void MidiStore::addNoteEventAtTime(int64 time, int note, bool isOn)
 }
 
 /**
+ * @brief Make sure a child exists at the specified time. If not add it in sorted order
+ * 
+ * @param time 
+ * @return ValueTree 
+ */
+ValueTree MidiStore::ensureNoteEventAtTime(int64 time) 
+{
+    Identifier timeProp = notesAtIdent(time);
+    const ScopedLock lock(storeLock);
+    ValueTree existingChild = trackData->getChildWithName(timeProp);
+    if (existingChild.isValid()) 
+        return existingChild;
+    
+    int pos;
+    if (time > maxTimeStored) 
+    {
+        // We can just add it at the end
+        pos = -1;
+        maxTimeStored = time;
+    }
+    else 
+    {
+        // super bad cheesy insertion sort ... assumption is that this is relatively uncommon
+        pos = 0;
+        for (ValueTree::Iterator events = trackData->begin(); events != trackData->end(); ++events)
+        {
+            ValueTree child = *events;
+            int64 eventTime = child.getProperty("eventTime");
+            jassert(eventTime != time);
+            if (eventTime > time)
+                break;
+            pos++;
+        }
+
+    }
+
+    ValueTree newChild(timeProp);
+    trackData->addChild(newChild, pos, nullptr);
+    return newChild;
+}
+
+
+/**
  * @brief Retrieve a sorted array of note values that have ON events at the given time.
  * This is the list of notes that have on events at the time (not necessarily all notes that are on)
- * 
- * @param int64 time 
- * @return Array<int> 
+ *
+ * @param int64 time
+ * @return std::vector<int>
  */
-Array<int> MidiStore::getNoteOnEventsAtTime(int64 time) 
+std::vector<int> MidiStore::getNoteOnEventsAtTime(int64 time)
 {
     Identifier timeProp = notesAtIdent(time);
     const ScopedLock lock(storeLock);
     ValueTree child = trackData->getChildWithName(timeProp);
-    Array<int> notes;
+    std::vector<int> notes;
     for (int i = 0; i < child.getNumProperties(); ++i)
     {
         Identifier noteIdent = child.getPropertyName(i);
-        int note = std::stoi(noteIdent.toString().toStdString());
+        String eventStr = noteIdent.toString();
+        int note;
+        // If this is not an integer, it is not a note event. Skip it in that case
+        if (!stringToInt(eventStr, &note))
+        {
+            continue;
+        }
         bool isOn = child.getProperty(noteIdent);
-        if (isOn) {
-            notes.add(note);
+        if (isOn)
+        {
+            notes.push_back(note);
         }
     }
-    // Need to figure out the nuances of the scopedlock ... ideally should release the lock here before the 
+
+    // Need to figure out the nuances of the scopedlock ... ideally should release the lock here before the
     // sort operation. Or maybe move the extraction call/loop above into a simple method and use the scoped
     // lock there.
-    
-    DefaultElementComparator<int> sorter;
-    notes.sort(sorter);
+    std::sort(notes.begin(), notes.end());
     return notes;
 }
 
@@ -113,69 +155,79 @@ Array<int> MidiStore::getNoteOnEventsAtTime(int64 time)
  * @brief Retrieve the list of notes that are ON at the given moment
  * This assumes that the child nodes in the tree are sorted by time
  * Current algorithm is brute force. Start at the beginning and track on/off events
- * 
- * @param int64 time 
- * @return Array<int>   Note values at that time
+ *
+ * @param int64 startTime     Ignore notes before this time
+ * @param int64 endTime       Point in time of interest
+ * @return std::vector<int>   Note values at that time
  */
-Array<int> MidiStore::getAllNotesOnAtTime(int64 time) 
+std::vector<int> MidiStore::getAllNotesOnAtTime(int64 startTime, int64 endTime)
 {
     const ScopedLock lock(storeLock);
     SortedSet<int> notes;
 
+    // Brute force for now. Start at the beginning and work through
     for (ValueTree::Iterator events = trackData->begin(); events != trackData->end(); ++events)
     {
         ValueTree child = *events;
         for (int i = 0; i < child.getNumProperties(); ++i)
         {
             int64 eventTime = child.getProperty("eventTime");
-            if (eventTime > time)
-            {
+            if (eventTime < startTime) 
+                continue;
+            if (eventTime > endTime)
                 break;
-            }
+
             Identifier noteIdent = child.getPropertyName(i);
             std::string eventStr = noteIdent.toString().toStdString();
+            int note;
             // If this is not an integer, it is not a note event. Skip it in that case
-            if (eventStr.find_first_not_of("0123456789") != std::string::npos)
-            {
-                DBG("Skipping non-note event");
+            if (!stringToInt(eventStr, &note))
                 continue;
-            }
-            else 
-            {
-                DBG("Found note event");
-            }
-            
-            int note = std::stoi(noteIdent.toString().toStdString());
+
             bool isOn = child.getProperty(noteIdent);
-            if (isOn) 
-            {
+            if (isOn)
                 notes.add(note);
-            }
-            else 
-            {
-                notes.remove(note);
-            }
+            else
+                notes.removeValue(note);
         }
-
     }
 
-    Array<int> returnVal;
-    for (int i = 0; i < notes.size(); i++) 
-    {
-        returnVal.add(notes[i]);
-    }
+    std::vector<int> returnVal;
+    for (int i = 0; i < notes.size(); i++)
+        returnVal.push_back(notes[i]);
 
     return returnVal;
-
 }
 
 /**
- * @brief Create a ValudTree identifier for notes at a given time
- * 
- * @param int64 time 
- * @return Identifier 
+ * @brief Convert a juce string to an integer
+ * Could just use the built-in getIntValue, but it returns 0 if not a string with no indicator 
+ * that I can see if it wasn't an integer
+ *
+ * @param str
+ * @param value
+ * @return bool  True if converted, false if not (e.g., not an integer)
  */
-Identifier MidiStore::notesAtIdent(int64 time) 
+bool MidiStore::stringToInt(String str, int *value)
+{
+    std::string stdstr = str.toStdString();
+    if (stdstr.find_first_not_of("0123456789") != std::string::npos)
+    {
+        // Not an integer apparently
+        return false;
+    }
+    *value = std::stoi(stdstr);
+    return true;
+}
+
+
+/**
+ * @brief Create a ValudTree identifier for notes at a given time
+ *
+ * @param int64 time
+ * @return Identifier
+ */
+Identifier MidiStore::notesAtIdent(int64 time)
 {
     std::ostringstream oss;
     oss << "notesat:" << time;
--- tests/midiStoreTest.cpp
+++ tests/midiStoreTest.cpp
@@ -1,8 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
 #include "MidiStore.h"
+#include <algorithm>
+#include <random>
 
-
-TEST_CASE( "midi store basics") {
+TEST_CASE("midi store basics")
+{
     MidiStore ms;
     REQUIRE(ms.hasData() == false);
     ms.setName("my new name");
@@ -10,7 +12,8 @@ TEST_CASE( "midi store basics") {
     REQUIRE(ms.hasData() == false);
     juce::String name = ms.getName();
     REQUIRE(name == "my new name");
-
+    ms.addNoteEventAtTime(50, 123, true);
+    REQUIRE(ms.hasData() == true);
 }
 
 TEST_CASE("note storage basics", "storage")
@@ -18,12 +21,13 @@ TEST_CASE("note storage basics", "storage")
     MidiStore ms;
 
     ms.addNoteEventAtTime(50, 123, true);
-    Array<int> notes = ms.getNoteOnEventsAtTime(50);
+    std::vector<int> notes = ms.getNoteOnEventsAtTime(50);
     REQUIRE(notes.size() == 1);
     REQUIRE(notes[0] == 123);
     ms.addNoteEventAtTime(50, 15, true);
     notes = ms.getNoteOnEventsAtTime(50);
     REQUIRE(notes.size() == 2);
+    // This verifies that the results are sorted
     REQUIRE(notes[0] == 15);
     REQUIRE(notes[1] == 123);
 
@@ -34,24 +38,24 @@ TEST_CASE("note storage basics", "storage")
     REQUIRE(notes[1] == 123);
 }
 
-TEST_CASE("empty note slot", "storage") 
+TEST_CASE("empty note slot", "storage")
 {
     MidiStore ms;
-    Array<int> notes = ms.getNoteOnEventsAtTime(3333);
+    std::vector<int> notes = ms.getNoteOnEventsAtTime(3333);
     REQUIRE(notes.size() == 0);
 }
 
-TEST_CASE("notes off", "storage") 
+TEST_CASE("notes off", "storage")
 {
     MidiStore ms;
 
     ms.addNoteEventAtTime(50, 120, false);
     ms.addNoteEventAtTime(50, 127, false);
-    Array<int> notes = ms.getNoteOnEventsAtTime(50);
+    std::vector<int> notes = ms.getNoteOnEventsAtTime(50);
     REQUIRE(notes.size() == 0);
 }
 
-TEST_CASE("notes on and off", "storage") 
+TEST_CASE("notes on and off", "storage")
 {
     MidiStore ms;
 
@@ -59,7 +63,103 @@ TEST_CASE("notes on and off", "storage")
     ms.addNoteEventAtTime(50, 120, false);
     ms.addNoteEventAtTime(50, 123, true);
     ms.addNoteEventAtTime(50, 127, false);
-    Array<int> notes = ms.getNoteOnEventsAtTime(50);
+    std::vector<int> notes = ms.getNoteOnEventsAtTime(50);
     REQUIRE(notes.size() == 1);
     REQUIRE(notes[0] == 123);
+}
+
+TEST_CASE("notes time case 2", "storage")
+{
+    MidiStore ms;
+    std::vector<int> notes;
+
+    // Corner case where the off is added out of order. This essentially
+    // verifies that events are kept in order in the MidiStore
+    ms.addNoteEventAtTime(15, 100, false);
+    ms.addNoteEventAtTime(10, 100, true);
+    ms.addNoteEventAtTime(20, 103, true);
+    notes = ms.getAllNotesOnAtTime(0, 200);
+    std::vector<int> expected = {103};
+    REQUIRE(notes == expected);
+}
+
+TEST_CASE("notes on simple", "storage")
+{
+    MidiStore ms;
+
+    std::vector<int> notes = {0};
+
+    notes = ms.getAllNotesOnAtTime(0, 25);
+    REQUIRE(notes.size() == 0);
+
+    ms.addNoteEventAtTime(50, 120, true);
+    notes = ms.getAllNotesOnAtTime(0, 25);
+    REQUIRE(notes.size() == 0);
+
+    notes = ms.getAllNotesOnAtTime(0, 50);
+    REQUIRE(notes.size() == 1);
+    REQUIRE(notes[0] == 120);
+    notes = ms.getAllNotesOnAtTime(0, 51);
+    REQUIRE(notes.size() == 1);
+    REQUIRE(notes[0] == 120);
+}
+
+// This is a simple chord sequence that provides a more "end to end" test of the class
+TEST_CASE("notes on random", "storage2")
+{
+    struct event
+    {
+        int64 time;
+        int note;
+        bool onoff;
+    };
+    MidiStore ms;
+    std::vector<event> events;
+    std::vector<int> notes;
+
+    events = {
+        // C
+        {10, 60, true},
+        {15, 64, true},
+        {20, 67, true},
+        // {30, 60, false},
+        {30, 64, false},
+        {30, 67, false},
+        // F
+        {30, 60, true},
+        {30, 65, true},
+        {30, 69, true},
+        // change it to Dm
+        {50, 62, true},
+        {50, 60, false}};
+
+    // Just to make it more interesting, shuffle it before adding to ensure that
+    // it is treated consistently. Note that the note off for 60 at time 20 is commented out
+    // above. If that is left in and it is added AFTER the the "subsequent" on event, then
+    // the MidiStore code treats it as "off" since last event wins. I'm not entirely sure
+    // what happens in the ProcessAudio callback for situations like that. I'm not sure
+    // it is actually possible in a midi track to have a note end and the same note start
+    // at exactly the same time. If so ... then ... well I'm still not sure what the result is
+    // supposed to be.
+    std::shuffle(events.begin(), events.end(), std::random_device());
+
+    for (std::vector<event>::iterator ev = events.begin(); ev != events.end(); ++ev)
+        ms.addNoteEventAtTime(ev->time, ev->note, ev->onoff);
+
+    notes = ms.getAllNotesOnAtTime(0, 15);
+    std::vector<int> expected = {60, 64};
+    REQUIRE(notes == expected);
+    notes = ms.getAllNotesOnAtTime(0, 20);
+    expected = {60, 64, 67};
+    REQUIRE(notes == expected);
+    notes = ms.getAllNotesOnAtTime(0, 30);
+    expected = {60, 65, 69};
+    REQUIRE(notes == expected);
+    notes = ms.getAllNotesOnAtTime(0, 50);
+    expected = {62, 65, 69};
+    REQUIRE(notes == expected);
+
+    notes = ms.getAllNotesOnAtTime(50, 50);
+    expected = {62};
+    REQUIRE(notes == expected);
 }
