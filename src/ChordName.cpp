0e5b0c95e85678c2d9dbc52e0df699a933295ff8
Mark Wilkins <markwilkins@github.com>
2023-05-27 15:29:31 -0600

    remove dev notes

--- src/ChordName.cpp
+++ src/ChordName.cpp
@@ -5,37 +5,6 @@
 #include <algorithm>
 using namespace std;
 
-/*
-To create an algorithm that produces a chord name given up to 5 notes, follow these steps: 
-1. Define the input: Accept up to 5 unique musical notes as input. For example, the input could be: C, E, G, B, D. 
-2. Normalize the input: Convert each note to its pitch class integer representation. In this system, 
-   C=0, C#=1, D=2, D#=3, E=4, F=5, F#=6, G=7, G#=8, A=9, A#=10, B=11. For example, the input C, E, G, B, D would be converted to 0, 4, 7, 11, 2. 
-3. Sort the input: Sort the pitch class integers in ascending order. If the input is already sorted, you can skip this step. 
-In our example, the sorted input would be 0, 2, 4, 7, 11. 
-4. Calculate intervals: Calculate the intervals between each consecutive pitch class integer in the sorted input. 
-   In our example, the intervals would be: 2, 2, 3, 4. 
-5. Identify the chord quality: Based on the intervals, determine the chord quality (e.g., major, minor, diminished, augmented, etc.). 
-   You can use the following common chord qualities and their corresponding interval patterns: 
-     - Major: 4, 3 
-     - Minor: 3, 4 
-     - Diminished: 3, 3 
-     - Augmented: 4, 4 
-     - Dominant 7th: 4, 3, 3 
-     - Major 7th: 4, 3, 4 
-     - Minor 7th: 3, 4, 3 
-     - Half-diminished 7th: 3, 3, 4 
-     - Diminished 7th: 3, 3, 3 
-     - Augmented 7th: 4, 4, 3 
-     - Minor-major 7th: 3, 4, 4 
-     - 9th, 11th, and 13th chords can be identified by extending the interval pattern. 
-   In our example, the interval pattern 2, 2, 3, 4 corresponds to a 9th chord. 
-6. Determine the root note: The first note in the sorted input is the root note. 
-   Convert the root note from its integer representation back to its musical note. 
-   In our example, the root note is 0, which corresponds to C. 
-7. Construct the chord name: Combine the root note and the chord quality to form the chord name. 
-   In our example, the chord name would be C9. The algorithm can be further refined to include more chord types and handle edge cases, 
-   but this serves as a starting point for producing a chord name given up to 5 notes.
-*/
 ChordName::ChordName()
 {
 }
